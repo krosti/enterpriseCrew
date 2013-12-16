34c77262559bef90f7171b459675d30e37a4749f
Gaston Mira <gastonmira@gmail.com>
2013-12-16 10:31:51 -0300

    Adding save function and edit function

--- js/app.js
+++ js/app.js
@@ -1,28 +1,63 @@
 var enterpriseApp = angular.module('enterprise', ['ngRoute']);
 
-enterpriseApp.config(function($routeProvider){
+enterpriseApp.config(function($routeProvider) {
 	$routeProvider.
-		when('/', {
-			templateUrl: 'partials/list.html'
-		})
+	when('/', {
+		templateUrl: 'partials/list.html'
+	})
 		.when('/new', {
-			templateUrl: 'partials/edit.html'
+			templateUrl: 'partials/edit.html',
+			controller: 'NewCtrl'
 		})
-		.otherwise({ redirectTo: '/' })
+		.when('/edit/:id', {
+			templateUrl: 'partials/edit.html',
+			controller: 'EditCtrl'
+		})
+		.otherwise({
+			redirectTo: '/'
+		})
 });
 
-enterpriseApp.controller('AppCtrl',['$scope', function ($scope){
-	$scope.crew = [{
-		name: "Picard",
-		description: "Captain"
-	}, {
-		name: "Messi",
-		description: "God"
-	}, {
-		name: "Ronaldo",
-		description: "I dont know"
-	}, {
-		name: "Maradona",
-		description: "God God!"
-	}];
-}]);
+enterpriseApp.controller('AppCtrl', ['$scope',
+	function($scope) {
+		$scope.crew = [{
+			name: "Picard",
+			description: "Captain"
+		}, {
+			name: "Messi",
+			description: "God"
+		}, {
+			name: "Ronaldo",
+			description: "I dont know"
+		}, {
+			name: "Maradona",
+			description: "God God!"
+		}];
+	}
+]);
+
+enterpriseApp.controller('NewCtrl', ['$scope', '$location',
+
+	function($scope, $location) {
+		$scope.person = {
+			name: "",
+			description: ""
+		},
+
+		$scope.save = function() {
+			$scope.crew.push($scope.person);
+			$location.path("/");
+		}
+	}
+]);
+
+enterpriseApp.controller('EditCtrl', ['$scope', '$location', '$routeParams',
+
+	function($scope, $location, $routeParams) {
+		$scope.person = $scope.crew[$routeParams.id];
+
+		$scope.save = function() {
+			$location.path("/");
+		}
+	},
+]);
